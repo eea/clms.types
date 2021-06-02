84b6d47f3b1d98265b52edb9fd51adb8e3621fb8
ionlizarazu <ilizarazu@codesyntax.com>
2021-06-02 14:41:31 +0200

    useCase content_type

--- clms/types/content/use_case.py
+++ clms/types/content/use_case.py
@@ -10,7 +10,31 @@ from plone.namedfile import field as namedfile
 from plone.supermodel import model
 from zope import schema
 from zope.interface import implementer
+from zope.schema.vocabulary import SimpleTerm
+from zope.schema.vocabulary import SimpleVocabulary
+from zope.schema.interfaces import IVocabularyFactory
+from zope.interface import provider
+from zope.component import adapter
+from zope.interface import Interface
 
+
+
+from plone import api
+
+
+
+@provider(IVocabularyFactory)
+def products_vocabulary_factory():
+    products = api.content.find(portal_type='Product')
+    productsList = [(p.getObject().id, p.getObject().title) for p in products]
+    terms = [SimpleTerm(value=pair[0], token=pair[0], title=pair[1]) for pair in productsList]
+    products_vocabulary = SimpleVocabulary(terms)
+    return products_vocabulary
+
+products_vocabulary = products_vocabulary_factory()
+
+class IUseCaseMarker(Interface):
+    pass
 
 class IUseCase(model.Schema):
     """ Marker interface for UseCase
@@ -20,6 +44,25 @@ class IUseCase(model.Schema):
     # and customize it in Python:
 
     # model.load('use_case.xml')
+
+    products = schema.List(
+    title=_(
+        u'CLMS products used',
+    ),
+    description=_(
+        u'Multiple selection allowed',
+    ),
+    value_type = schema.Choice(
+        title=_(
+            u'CLMS products used',
+        ),
+        vocabulary=products_vocabulary,
+        required=True,
+        readonly=False,
+    ),
+    required=True,
+    readonly=False,
+    )
 
     responsibleOrganization = schema.TextLine(
         title=_(u"Responsible organization"),
@@ -35,18 +78,32 @@ class IUseCase(model.Schema):
         title=_(u"Contact person email"),
         required=False,
     )
-    # Make sure to import: plone.app.vocabularies as vocabs
-    # topics = schema.Choice(
-    #     title=_(
-    #         u'Use case topics',
-    #     ),
-    #     vocabulary=u'vocabs.PortalTypes',
-    #     default=u'',
-    #     required=True,
-    #     readonly=False,
-    # )
 
-    name = schema.List(
+    topics = schema.List(
+    title=_(
+        u'Use case topics',
+    ),
+    description=_(
+        u'Multiple selection allowed',
+    ),
+    value_type = schema.Choice(
+        title=_(
+            u'Use case topics',
+        ),
+        vocabulary=u'clms.types.TopicsVocabulary',
+        required=True,
+        readonly=False,
+    ),
+    required=True,
+    readonly=False,
+    )
+
+    outcome = schema.TextLine(
+        title=_(u"User case outcome"),
+        required=True,
+    )
+
+    links = schema.List(
         title=_(
             u'Links to use case',
         ),
@@ -66,5 +123,89 @@ class IUseCase(model.Schema):
     )
 
 @implementer(IUseCase)
+@adapter(IUseCaseMarker)
 class UseCase(Container):
     """ UseCase content-type class """
+    def __init__(self, context):
+        self.context = context
+    
+    @property 
+    def products(self):
+        if hasattr(self.context, "products"):
+            return self.context.products
+        return None
+
+    @products.setter
+    def products(self, value):
+        self.context.products = value
+
+
+    @property 
+    def responsibleOrganization(self):
+        if hasattr(self.context, "responsibleOrganization"):
+            return self.context.responsibleOrganization
+        return None
+    @responsibleOrganization.setter
+    def responsibleOrganization(self, value):
+        self.context.responsibleOrganization = value
+
+
+    @property 
+    def contactName(self):
+        if hasattr(self.context, "contactName"):
+            return self.context.contactName
+        return None
+    @contactName.setter
+    def contactName(self, value):
+        self.context.contactName = value
+
+
+    @property 
+    def contactEmail(self):
+        if hasattr(self.context, "contactEmail"):
+            return self.context.contactEmail
+        return None
+    @contactEmail.setter
+    def contactEmail(self, value):
+        self.context.contactEmail = value
+
+
+    @property 
+    def topics(self):
+        if hasattr(self.context, "topics"):
+            return self.context.topics
+        return None
+    @topics.setter
+    def topics(self, value):
+        self.context.topics = value
+
+
+    @property 
+    def outcome(self):
+        if hasattr(self.context, "outcome"):
+            return self.context.outcome
+        return None
+    @outcome.setter
+    def outcome(self, value):
+        self.context.outcome = value
+
+
+    @property 
+    def links(self):
+        if hasattr(self.context, "links"):
+            return self.context.links
+        return None
+    @links.setter
+    def links(self, value):
+        self.context.links = value
+
+
+    @property 
+    def image(self):
+        if hasattr(self.context, "image"):
+            return self.context.image
+        return None
+    @image.setter
+    def image(self, value):
+        self.context.image = value
+
