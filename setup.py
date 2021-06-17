4292283da3321d9e6fdb38f09f41f9b0a38a5f48
Mikel Larreategi <mlarreategi@codesyntax.com>
2021-06-17 14:22:18 +0200

    black formatting

--- setup.py
+++ setup.py
@@ -46,7 +46,11 @@ setup(
         "plone.restapi",
         "collective.volto.dropdownmenu",
     ],
-    extras_require={"test": ["plone.app.testing",],},
+    extras_require={
+        "test": [
+            "plone.app.testing",
+        ],
+    },
     entry_points="""
     [z3c.autoinclude.plugin]
     target = plone
