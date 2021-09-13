96c3850fb9233fd1b748b4276dfcda4afc4aab8d
Lur Ibargutxi <libargutxi@codesyntax.com>
2021-09-13 16:41:54 +0200

    technical library content-type and indexers

---
+++ clms/types/content/technical_library.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+TechnicalLibrary content-type definition
+"""
+import json
+
+from plone.app.textfield import RichText
+from plone.dexterity.content import Container
+from plone.namedfile import field as namedfile
+from plone.schema.jsonfield import JSONField
+from plone.supermodel import model
+from zope import schema
+from zope.interface import implementer
+from plone.namedfile.field import NamedBlobFile
+
+from clms.types import _
+
+
+class ITechnicalLibrary(model.Schema):
+    """Marker interface and Dexterity Python Schema for TechnicalLibrary"""
+
+    # If you want, you can load a xml model created TTW here
+    # and customize it in Python:
+
+    # model.load('product.xml')
+    file = NamedBlobFile(title=_(u"File"), required=True)
+
+    products = schema.List(
+        title=_(
+            u"CLMS associated products",
+        ),
+        description=_(
+            u"Multiple selection allowed",
+        ),
+        value_type=schema.Choice(
+            title=_(
+                u"CLMS products used",
+            ),
+            vocabulary=u"clms.types.ProductsVocabulary",
+            required=True,
+            readonly=False,
+        ),
+        required=True,
+        readonly=False,
+    )
+
+
+@implementer(ITechnicalLibrary)
+class TechnicalLibrary(Container):
+    """TechnicalLibrary content-type class"""
---
+++ clms/types/indexers/associated_products.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from plone.dexterity.interfaces import IDexterityContent
+from plone.indexer import indexer
+from clms.types.content.technical_library import ITechnicalLibrary
+
+
+@indexer(IDexterityContent)
+def dummy(obj):
+    """Dummy to prevent indexing other objects thru acquisition"""
+    raise AttributeError("This field should not indexed here!")
+
+
+@indexer(ITechnicalLibrary)
+def associated_products(obj):
+    """Calculate and return the value for the indexer"""
+    return obj.products
