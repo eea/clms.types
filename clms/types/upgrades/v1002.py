02a6d23e53c2c86358bf15c81b77f6f7cdd1da4b
ionlizarazu <ilizarazu@codesyntax.com>
2021-09-20 15:47:46 +0200

    pep8 and pylint

--- clms/types/upgrades/v1002.py
+++ clms/types/upgrades/v1002.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
+"""Run upgrade"""
 
 from . import logger
 
+
 def upgrade(setup_tool=None):
-    """"""
+    """Run upgrade"""
     logger.info(
-        "Running upgrade (Python): Reinstall profile to be sure to set the LRF schema"
+        "Running upgrade (Python): "
+        "Reinstall profile to be sure to set the LRF schema"
     )
     logger.info("Done")
