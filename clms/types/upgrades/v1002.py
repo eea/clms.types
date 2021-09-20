16cb14e247512cbe247912cce9e4c96bd880fbad
ionlizarazu <ilizarazu@codesyntax.com>
2021-09-20 15:28:47 +0200

    remove unneeded code

--- clms/types/upgrades/v1002.py
+++ clms/types/upgrades/v1002.py
@@ -2,16 +2,9 @@
 
 from . import logger
 
-
-from .base import reload_gs_profile
-
-# from plone import api
-
-
 def upgrade(setup_tool=None):
     """"""
     logger.info(
         "Running upgrade (Python): Reinstall profile to be sure to set the LRF schema"
     )
-    reload_gs_profile(setup_tool)
     logger.info("Done")
