17dafdd840208e73b9d0622577ae7ef54f3da3b0
Mikel Larreategi <mlarreategi@codesyntax.com>
2021-11-30 09:23:20 +0100

    upgrade step to install the component behavior

---
+++ clms/types/upgrades/v1006.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""Run upgrade"""
+
+from plone import api
+
+from . import logger
+
+
+def upgrade(setup_tool=None):
+    """Run upgrade"""
+    logger.info("Running upgrade (Python): " "Add new content type")
+    logger.info("Done")
