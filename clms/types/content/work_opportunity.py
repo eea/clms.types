2e761c2da39a61ee9815943e991abd680005252b
ionlizarazu <ilizarazu@codesyntax.com>
2022-01-10 09:01:14 +0100

    unused imports and import grouping

--- clms/types/content/work_opportunity.py
+++ clms/types/content/work_opportunity.py
@@ -5,9 +5,9 @@ WorkOpportunity content-type definition
 from plone.dexterity.content import Container
 from plone.supermodel import model
 from zope.interface import implementer
-from zope import schema
+# from zope import schema
 
-from clms.types import _
+# from clms.types import _
 
 
 class IWorkOpportunity(model.Schema):
--- clms/types/vocabularies/update_frequency_vocabulary.py
+++ clms/types/vocabularies/update_frequency_vocabulary.py
@@ -4,13 +4,13 @@ Category Topics vocabulary definition
 """
 
 from plone import api
-from clms.types import _
 from plone.dexterity.interfaces import IDexterityContent
 from zope.globalrequest import getRequest
 from zope.interface import implementer
 from zope.schema.interfaces import IVocabularyFactory
-from zope.schema.vocabulary import SimpleTerm
-from zope.schema.vocabulary import SimpleVocabulary
+from zope.schema.vocabulary import SimpleTerm, SimpleVocabulary
+
+# from clms.types import _
 
 
 class VocabItem:
