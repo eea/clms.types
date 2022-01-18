b94f97b4cb7c4c5a7e8b9488f59a5d7ecaab5662
Mikel Larreategi <mlarreategi@codesyntax.com>
2022-01-18 19:45:05 +0100

    format measure units that come as an xml namespace values

--- clms/types/restapi/geonetwork/import_from_geonetwork.py
+++ clms/types/restapi/geonetwork/import_from_geonetwork.py
@@ -694,9 +694,8 @@ class ImportFromGeoNetwork(Service):
                 elif field["type"] == "resolution":
                     item = fields_data[0]
                     resolution = item.attrib.get(field.get("attribute"))
-                    if (
-                        resolution.startswith("http") and resolution.find("#") != -1
-                    ):
+                    # pylint: disable=line-too-long
+                    if resolution.startswith("http") and resolution.find("#") != -1:  # noqa: E501
                         resolution = resolution.split("#")[1]
                     result[field["field_id"]] = {
                         "data": f"{item.text} {resolution}",
