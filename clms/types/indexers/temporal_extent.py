53cb98ebcfdad0630f9e0f634a0e47b20d085f8d
Mikel Larreategi <mlarreategi@codesyntax.com>
2022-01-18 16:53:51 +0100

    new indexes

---
+++ clms/types/indexers/temporal_extent.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""Associated products indexers"""
+
+from plone.dexterity.interfaces import IDexterityContent
+from plone.indexer import indexer
+from clms.types.content.data_set import IDataSet
+from datetime import datetime
+
+
+@indexer(IDexterityContent)
+def dummy(obj):
+    """Dummy to prevent indexing other objects thru acquisition"""
+    raise AttributeError("This field should not indexed here!")
+
+
+@indexer(IDataSet)
+def temporal_extent(obj):
+    """Calculate and return the value for the indexer"""
+    try:
+        dt_temporalExtentStart = datetime.strptime(
+            obj.temporalExtentStart, "%Y-%m-%d"
+        )
+    except:
+        return []
+
+    try:
+        dt_temporalExtentEnd = datetime.strptime(
+            obj.temporalExtentEnd, "%Y-%m-%d"
+        )
+    except:
+        dt_temporalExtentEnd = datetime.now()
+
+    try:
+        return range(
+            dt_temporalExtentStart.year, dt_temporalExtentEnd.year + 1
+        )
+    except:
+        return []
