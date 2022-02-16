8a6f2c5b9df445370001dd98b94c23ba0239c817
Mikel Larreategi <mlarreategi@codesyntax.com>
2022-02-16 13:08:55 +0100

    linter

--- clms/types/tests/test_category_topics_vocabulary.py
+++ clms/types/tests/test_category_topics_vocabulary.py
@@ -10,7 +10,6 @@ from plone.app.testing import TEST_USER_ID, setRoles
 from zope.component import getUtility
 from zope.schema.interfaces import IVocabularyFactory, IVocabularyTokenized
 
-from clms.types import _
 from clms.types.testing import CLMS_TYPES_FUNCTIONAL_TESTING
 
 
