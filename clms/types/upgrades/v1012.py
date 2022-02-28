8ff91df03e6bb50f0f3fd37d86942e381064fe8a
Mikel Larreategi <mlarreategi@codesyntax.com>
2022-02-28 14:48:38 +0100

    new field to index in the catalog

---
+++ clms/types/upgrades/v1012.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""Run upgrade"""
+
+from plone import api
+
+from . import logger
+
+
+def upgrade(setup_tool=None):
+    """Run upgrade"""
+    logger.info("Running upgrade (Python): New indexes and catalog fields")
+    setup = api.portal.get_tool("portal_setup")
+    setup.runImportStepFromProfile("clms.types:default", "catalog")
+    setup.runImportStepFromProfile("clms.types:default", "plone.app.registry")
+    logger.info("Done")
