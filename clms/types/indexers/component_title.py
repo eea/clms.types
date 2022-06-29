8c3142c3b7ba813832558f142313ac48b2a4d421
Mikel Larreategi <mlarreategi@codesyntax.com>
2022-06-29 11:42:06 +0200

    lint

--- clms/types/indexers/component_title.py
+++ clms/types/indexers/component_title.py
@@ -3,8 +3,6 @@
 
 from plone.dexterity.interfaces import IDexterityContent
 from plone.indexer import indexer
-from zope.component import getUtility
-from zope.schema.interfaces import IVocabularyFactory
 
 from clms.types.behaviors.mapviewer_component import IMapviewerComponentMarker
 
@@ -20,12 +18,3 @@ def component_title_behavior(obj):
     """Calculate and return the value for the indexer"""
 
     return obj.mapviewer_component
-    # component = obj.mapviewer_component
-    # vocabulary = getUtility(
-    #     IVocabularyFactory, name="clms.types.ComponentTitleVocabulary"
-    # )
-    # terms = vocabulary(obj)
-    # try:
-    #     return terms.getTerm(component).title
-    # except LookupError:
-    #     return ""
