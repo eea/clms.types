07efed7cdf637f4f3bab7d48255a7d7ce04f2078
Mikel Larreategi <mlarreategi@codesyntax.com>
2022-07-13 12:42:57 +0200

    fixes

--- clms/types/content/utils.py
+++ clms/types/content/utils.py
@@ -1,28 +1,16 @@
+""" some utils"""
 # -*- coding: utf-8 -*-
-from Products.CMFPlone.RegistrationTool import EmailAddressInvalid
-from Products.CMFPlone.RegistrationTool import checkEmailAddress
-from z3c.form import validator
-from zope import schema
-
-from clms.types import _
+from Products.CMFPlone.RegistrationTool import (
+    EmailAddressInvalid,
+    checkEmailAddress,
+)
 
 
 def valid_email(value):
+    """return whether the value is a valid email or not"""
     if value is not None:
         try:
             checkEmailAddress(value)
         except EmailAddressInvalid:
             return False
     return True
-
-
-class WrongEmail(schema.ValidationError):
-    __doc__ = _("""The entered email is not valid""")
-
-
-class EmailValidator(validator.SimpleFieldValidator):
-    def validate(self, value):
-        super(EmailValidator, self).validate(value)
-        if valid_email(value):
-            return True
-        raise WrongEmail
