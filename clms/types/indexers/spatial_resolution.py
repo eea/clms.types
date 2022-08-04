c31d9ecf453d98ccff7ad753ea4c1a99993fd8ef
Mikel Larreategi <mlarreategi@codesyntax.com>
2022-08-04 09:15:30 +0200

    convert spatial resolutions to meters

--- clms/types/indexers/spatial_resolution.py
+++ clms/types/indexers/spatial_resolution.py
@@ -4,6 +4,28 @@
 from plone.dexterity.interfaces import IDexterityContent
 from plone.indexer import indexer
 from clms.types.content.data_set import IDataSet
+from decimal import Decimal
+
+
+def convert_spatial_resolutions(values):
+    """convert spatial resolution values from degrees to meters"""
+    new_values = []
+    for value in values:
+        if value and value.strip().endswith("deg"):
+            new_values.append(convert_spatial_resolution(value))
+        else:
+            new_values.append(value)
+
+    return new_values
+
+
+def convert_spatial_resolution(value):
+    """convert from degrees to meters"""
+    numeric_value = value.replace("deg", "").strip()
+    decimal_numeric_value = Decimal(numeric_value)
+    decimal_numeric_value_quantized = decimal_numeric_value.quantize(Decimal('1.0000'))
+    result = decimal_numeric_value_quantized * Decimal('10007566.8') / 90
+    return f'{result.to_integral()} m'
 
 
 @indexer(IDexterityContent)
@@ -16,4 +38,4 @@ def dummy(obj):
 def spatial_resolution(obj):
     """Calculate and return the value for the indexer"""
     items = obj.qualitySpatialResolution_line
-    return items.split(",") if items else []
+    return convert_spatial_resolutions(items.split(",") if items else [])
