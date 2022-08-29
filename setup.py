00c91508c37106a9f7c79d2bae9be1daa2cb254e
Mikel Larreategi <mlarreategi@codesyntax.com>
2022-08-29 09:47:02 +0200

    changes in spatial coverage vocabulary, mark items for future deletion

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     namespace_packages=["clms"],
     include_package_data=True,
     zip_safe=False,
-    python_requires="==2.7, >=3.6",
+    python_requires=">=3.6",
     install_requires=[
         "setuptools",
         "collective.z3cform.datagridfield",
