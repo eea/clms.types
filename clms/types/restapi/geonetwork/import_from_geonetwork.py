cd4606a57801e0ccc437ee27f3435976d9d96459
Mikel Larreategi <mlarreategi@codesyntax.com>
2022-09-14 12:36:14 +0200

    lint

--- clms/types/restapi/geonetwork/import_from_geonetwork.py
+++ clms/types/restapi/geonetwork/import_from_geonetwork.py
@@ -655,10 +655,7 @@ class ImportFromGeoNetwork(Service):
                     item = fields_data[0]
                     resolution = item.attrib.get(field.get("attribute"))
                     # pylint: disable=line-too-long
-                    if (
-                        resolution.startswith("http")
-                        and resolution.find("#") != -1
-                    ):  # noqa: E501
+                    if (resolution.startswith("http") and resolution.find("#") != -1):  # noqa: E501
                         resolution = resolution.split("#")[1]
                     result[field["field_id"]] = {
                         "data": f"{item.text} {resolution}",
