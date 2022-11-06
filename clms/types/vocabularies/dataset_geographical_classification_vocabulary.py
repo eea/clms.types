91f263ee95120f2366a33bedf95e6ae183b5def8
Mikel Larreategi <mlarreategi@codesyntax.com>
2022-11-06 17:38:14 +0100

    new index and upgrade step

---
+++ clms/types/vocabularies/dataset_geographical_classification_vocabulary.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Spatial representation type vocabulary definition
+"""
+
+from plone import api
+from zope.interface import implementer
+from zope.schema.interfaces import IVocabularyFactory
+from zope.schema.vocabulary import SimpleTerm
+from zope.schema.vocabulary import SimpleVocabulary
+
+
+class VocabItem:
+    """
+    VocabItem class
+    """
+
+    def __init__(self, token, value):
+        self.token = token
+        self.value = value
+
+
+@implementer(IVocabularyFactory)
+class DatasetGeographicalClassificationVocabulary:
+    """
+    CategoryTopics vocabulary class
+    """
+
+    def __call__(self, context):
+        # Just an example list of content for our vocabulary,
+        # this can be any static or dynamic data, a catalog result for example.
+        catalog = api.portal.get_tool("portal_catalog")
+        items = catalog.uniqueValuesFor("dataset_geographical_classification")
+
+        # create a list of SimpleTerm items:
+        terms = []
+        for item in items:
+            terms.append(
+                SimpleTerm(
+                    value=item,
+                    token=str(item),
+                    title=item,
+                )
+            )
+        # Create a SimpleVocabulary from the terms list and return it:
+        return SimpleVocabulary(terms)
+
+
+DatasetGeographicalClassificationVocabularyFactory = DatasetGeographicalClassificationVocabulary()
