8f859b03f183079176f1c5709d84b72f1cba10e0
Mikel Larreategi <mlarreategi@codesyntax.com>
2022-12-21 09:34:03 +0100

    changes

--- clms/types/upgrades/v1023.py
+++ clms/types/upgrades/v1023.py
@@ -1,9 +1,20 @@
 # -*- coding: utf-8 -*-
 """Run upgrade"""
 from . import logger
+from plone import api
+
+
+def remove_unneeded_behaviors():
+    """remove unneeded behaviors"""
+    pt = api.portal.get_tool("portal_types")
+    fti = pt.getTypeInfo("FAQ")
+    behaviors_list = list(fti.behaviors)
+    behaviors_list.remove("volto.blocks")
+    fti.behaviors = tuple(behaviors_list)
 
 
 def upgrade(setup_tool=None):
     """Run upgrade"""
     logger.info("Running upgrade (Python): v1023")
+    remove_unneeded_behaviors()
     logger.info("Done")
