39acc6674361e7db05370cb35bf3529c619c2d95
Mikel Larreategi <mlarreategi@codesyntax.com>
2023-03-05 21:50:14 +0100

    Plone 6 upgrade

---
+++ clms/types/upgrades/v1024.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""Run upgrade"""
+from . import logger
+
+
+def upgrade(setup_tool=None):
+    """Run upgrade"""
+    logger.info("Running upgrade (Python): v1024")
+    logger.info("Done")
