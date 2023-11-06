3162bdc4d976284f2cda8dc97d9700822594b163
Mikel Larreategi <mlarreategi@codesyntax.com>
2023-11-06 15:27:29 +0100

    feat: custom adapter to decide when to show the full link

---
+++ clms/types/restapi/adapters/technical_library.py
@@ -0,0 +1,30 @@
+"""
+Custom adapter to decide where to link when selecting
+a TechnicalLibrary item as a link target.
+
+It should behave as a normal link to a File except when
+the 'ondemand' attribute is set to True.
+
+In such case, the link should point to the content itself
+"""
+# -*- coding: utf-8 -*-
+from clms.types.content.technical_library import ITechnicalLibrary
+from plone.namedfile.interfaces import INamedFileField
+from plone.restapi.serializer.dxfields import PrimaryFileFieldTarget
+from zope.component import adapter
+from zope.interface import Interface
+
+
+@adapter(INamedFileField, ITechnicalLibrary, Interface)
+class TechnicalLibraryPrimaryFileFieldTarget(PrimaryFileFieldTarget):
+    """ Custom adapter for TechnicalLibrary items, to decide when to show
+        the primary field link
+    """
+    def use_primary_field_target(self):
+        """ alter condition"""
+        super_value = super(TechnicalLibraryPrimaryFileFieldTarget, self).use_primary_field_target()
+        if super_value:
+            if hasattr(self.context, 'ondemand') and self.context.ondemand:
+                return False
+            return True
+        return False
