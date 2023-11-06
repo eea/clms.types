6f64f5caf390272cf0e7d347649fa8c9649f7cad
Mikel Larreategi <mlarreategi@codesyntax.com>
2023-11-06 15:30:04 +0100

    lint

--- clms/types/restapi/adapters/technical_library.py
+++ clms/types/restapi/adapters/technical_library.py
@@ -22,7 +22,10 @@ class TechnicalLibraryPrimaryFileFieldTarget(PrimaryFileFieldTarget):
     """
     def use_primary_field_target(self):
         """ alter condition"""
-        super_value = super(TechnicalLibraryPrimaryFileFieldTarget, self).use_primary_field_target()
+        super_value = super(
+            TechnicalLibraryPrimaryFileFieldTarget,
+            self
+        ).use_primary_field_target()
         if super_value:
             if hasattr(self.context, 'ondemand') and self.context.ondemand:
                 return False
