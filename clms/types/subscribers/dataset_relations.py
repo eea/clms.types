d64552f92ecec367e0a0479e7f8cbaebd58d52fa
Mikel Larreategi <mlarreategi@codesyntax.com>
2024-01-18 16:42:54 +0100

    feat: CLMS-2868 implement to-way-automatic relation of datasets

---
+++ clms/types/subscribers/dataset_relations.py
@@ -0,0 +1,51 @@
+"""
+An event subsriber to set both-direction relations in datasets
+"""
+# -*- coding: utf-8 -*-
+from plone import api
+
+
+from logging import getLogger
+log = getLogger(__name__)
+
+
+def dataset_edit_handler(object, event):
+    """ handler executed when a dataset is edited"""
+    for description in event.descriptions:
+        if 'IDataSet.datasets' in description.attributes:
+            log.info('Changed')
+            # only execute if related datasets field
+            # have been modified
+
+            # First reset all existing relations
+            dataset_brains = api.content.find(
+                associated_datasets=object.UID(),
+                portal_type='DataSet'
+            )
+            for dataset_brain in dataset_brains:
+                dataset_object = dataset_brain.getObject()
+                try:
+                    dataset_object.datasets.remove(object.UID())
+                    log.info('Dataset relation removed in %s', dataset_object.absolute_url())
+                except ValueError:
+                    # The item is not there
+                    log.info('Dataset not related')
+                dataset_object.reindexObject()
+
+        # Finally do the setting again
+        for dataset_uid in object.datasets:
+            dataset = api.content.get(UID=dataset_uid)
+            if dataset.datasets is None:
+                dataset.datasets = []
+            if object.UID() not in dataset.datasets:
+                dataset.datasets.append(object.UID())
+                log.info(
+                    'Added related dataset. Current object %s, '
+                    'related dataset %s', object.absolute_url(), dataset.absolute_url()
+                )
+                dataset.reindexObject()
+
+
+
+
+
---
+++ clms/types/upgrades/v1032.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""Run upgrade"""
+from . import logger
+from plone import api
+
+
+def set_related_datasets():
+    """
+    set all dataset relations correctly
+    all related datasets should be related in both directions
+    """
+    brains = api.content.find(portal_type="DataSet")
+    for brain in brains:
+        current_dataset = brain.getObject()
+        if current_dataset.datasets is None:
+            current_dataset.datasets = []
+        for dataset_uid in current_dataset.datasets:
+            dataset = api.content.get(UID=dataset_uid)
+            if dataset.datasets is None:
+                dataset.datasets = []
+            if current_dataset.UID() not in dataset.datasets:
+                dataset.datasets.append(current_dataset.UID())
+                logger.info(
+                    "Added related dataset. Current object %s, "
+                    "related dataset %s",
+                    current_dataset.UID(),
+                    dataset_uid,
+                )
+                dataset.reindexObject()
+
+
+def upgrade(setup_tool=None):
+    """Run upgrade"""
+    logger.info("Running upgrade (Python): v1032")
+    set_related_datasets()
+    logger.info("Done")
