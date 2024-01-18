6a23151155d0894da2f4dd85885a579ac709d737
Mikel Larreategi <mlarreategi@codesyntax.com>
2024-01-18 16:47:29 +0100

    lint

--- clms/types/subscribers/dataset_relations.py
+++ clms/types/subscribers/dataset_relations.py
@@ -9,7 +9,7 @@ from plone import api
 log = getLogger(__name__)
 
 
-def dataset_edit_handler(object, event):
+def dataset_edit_handler(obj, event):
     """ handler executed when a dataset is edited"""
     for description in event.descriptions:
         if 'IDataSet.datasets' in description.attributes:
@@ -19,13 +19,13 @@ def dataset_edit_handler(object, event):
 
             # First reset all existing relations
             dataset_brains = api.content.find(
-                associated_datasets=object.UID(),
+                associated_datasets=obj.UID(),
                 portal_type='DataSet'
             )
             for dataset_brain in dataset_brains:
                 dataset_object = dataset_brain.getObject()
                 try:
-                    dataset_object.datasets.remove(object.UID())
+                    dataset_object.datasets.remove(obj.UID())
                     log.info(
                         'Dataset relation removed in %s',
                         dataset_object.absolute_url()
@@ -36,16 +36,16 @@ def dataset_edit_handler(object, event):
                 dataset_object.reindexObject()
 
         # Finally do the setting again
-        for dataset_uid in object.datasets:
+        for dataset_uid in obj.datasets:
             dataset = api.content.get(UID=dataset_uid)
             if dataset.datasets is None:
                 dataset.datasets = []
-            if object.UID() not in dataset.datasets:
-                dataset.datasets.append(object.UID())
+            if obj.UID() not in dataset.datasets:
+                dataset.datasets.append(obj.UID())
                 log.info(
                     'Added related dataset. Current object %s, '
                     'related dataset %s',
-                    object.absolute_url(),
+                    obj.absolute_url(),
                     dataset.absolute_url()
                 )
                 dataset.reindexObject()
