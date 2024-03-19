8e3dda306e2dc70781ba2343db4369197cf917b2
Mikel Larreategi <mlarreategi@codesyntax.com>
2024-03-19 12:49:56 +0100

    lint

--- clms/types/content/production_update.py
+++ clms/types/content/production_update.py
@@ -2,7 +2,6 @@
 """
 ProductionUpdate content-type definition
 """
-from plone import api
 from plone.dexterity.content import Container
 from plone.supermodel import model
 from zope import schema
@@ -12,7 +11,6 @@ from clms.types import _
 
 class IProductionUpdate(model.Schema):
     """Marker interface for ProductionUpdate"""
-
 
     status = schema.Choice(
         title=_(
@@ -29,4 +27,3 @@ class IProductionUpdate(model.Schema):
 @implementer(IProductionUpdate)
 class ProductionUpdate(Container):
     """UseCase content-type class"""
-
