13744ba9c7bb8992992c2fb4452f276cf01fbcd8
Mikel Larreategi <mlarreategi@codesyntax.com>
2024-03-19 15:28:47 +0100

    change

--- clms/types/content/product.py
+++ clms/types/content/product.py
@@ -12,18 +12,6 @@ from zope.interface import implementer
 class IProduct(model.Schema):
     """Marker interface and Dexterity Python Schema for Product"""
 
-    mapviewer_message = schema.TextLine(
-        title=_(
-            "Message to be shown next to the product name in the data viewer",
-        ),
-        description=_(
-            "",
-        ),
-        default="",
-        required=False,
-        readonly=False,
-    )
-
 
 @implementer(IProduct)
 class Product(Container):
