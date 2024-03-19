8bb695622cf3106abebecf73f84dc70b05b681ab
Mikel Larreategi <mlarreategi@codesyntax.com>
2024-03-19 16:09:51 +0100

    new index

---
+++ clms/types/indexers/dataset_characteristics_projections.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""Associated products indexers"""
+
+from plone.dexterity.interfaces import IDexterityContent
+from plone.indexer import indexer
+from clms.types.content.data_set import IDataSet
+
+
+@indexer(IDexterityContent)
+def dummy(obj):
+    """Dummy to prevent indexing other objects thru acquisition"""
+    raise AttributeError("This field should not indexed here!")
+
+
+@indexer(IDataSet)
+def dataset_characteristics_projections(obj):
+    """Calculate and return the value for the indexer"""
+    dataset_projection = obj.characteristics_projection
+    projections = dataset_projection.split("/")
+
+    cleaned_projections = map(clean, projections)
+    return cleaned_projections
+
+
+def clean(item):
+    """clean a EPSG value"""
+    new_item = item.strip()
+    if not new_item.startswith("EPSG"):
+        new_item = f"EPSG:{new_item}"
+
+    return new_item
